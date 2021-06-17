2923b0b9f3bf75711ff95585d91184272be08aea
VinciPy <viniciusantana323@gmail.com>
2021-06-17 19:30:32 -0300

    feat: NetBeans init src

---
+++ NetBeans SRC/src/avalaciao_2_poo/Avalaciao_2_POO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalaciao_2_poo;
+
+/**
+ *
+ * @author VinciPy
+ */
+public class Avalaciao_2_POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
