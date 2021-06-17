e8d239a0ff53abbbe417462ea04c3fe92a4df940
HederCarlosMilani <84051412+HederCarlosMilani@users.noreply.github.com>
2021-06-17 19:36:01 -0300

    Ra heder

--- NetBeans SRC/src/avalaciao_2_poo/Avalaciao_2_POO.java
+++ NetBeans SRC/src/avalaciao_2_poo/Avalaciao_2_POO.java
@@ -1,14 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    Heder Carlos Milani - 00134684
+*/
 package avalaciao_2_poo;
 
-/**
- *
- * @author VinciPy
- */
 public class Avalaciao_2_POO {
 
     /**
