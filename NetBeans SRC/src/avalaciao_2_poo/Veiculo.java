f6d862b8c38677cde3fcd3be35680ad641f25882
HederCarlosMilani <84051412+HederCarlosMilani@users.noreply.github.com>
2021-06-17 20:07:08 -0300

    Criação veiculo

---
+++ NetBeans SRC/src/avalaciao_2_poo/Veiculo.java
@@ -0,0 +1,34 @@
+
+package avalaciao_2_poo;
+
+/**
+ * @author hedermilani
+ */
+public abstract class Veiculo {
+    public boolean ligado;
+    public String marca, modelo;
+    public int marcha, qtdeMarchas;
+    
+    public boolean ligado(){
+      return ligado == true;
+    }
+    public boolean desligado(){
+        return ligado == false;
+    }
+    public void marchaAbaixo(){
+      if(marcha > 0){
+       marcha--;
+      }
+      else{
+         System.out.println("Não pode mais reduzir a marcha");
+      }
+    }
+     public void marchaAcima(){
+        if (marcha < qtdeMarchas) {
+            marcha ++;
+        }
+        else {
+            System.out.println("Marcha máxima atingida.");
+        }
+     }
+}
