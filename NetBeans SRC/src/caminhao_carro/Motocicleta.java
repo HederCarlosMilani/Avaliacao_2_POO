60825e3bdf1af72b2faf48e2863544266c85a032
HederCarlosMilani <84051412+HederCarlosMilani@users.noreply.github.com>
2021-06-17 20:47:10 -0300

    Create Motocicleta.java

---
+++ NetBeans SRC/src/caminhao_carro/Motocicleta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caminhao_carro;
+
+import avaliacao_2_poo.Veiculo;
+
+public class Motocicleta extends Veiculo{
+    
+    public Motocicleta(boolean ligado, String marca, String modelo, int marcha, int qtdeMarcas) {
+        super(ligado, marca, modelo, marcha, qtdeMarcas);
+    }
+    
+}
